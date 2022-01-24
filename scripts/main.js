239b2f923e82782ba85067634456315a126ba39a
Noah Taggart <noahtag@gmail.com>
2022-01-24 15:00:28 -0600

    filtered list of NY and Manufacturing companies

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,44 @@
-import { getBusinesses } from "./database.js";
+import { getBusinesses, newYorkCompanies, manufacturingCompanies } from "./database.js";
 import { businessHTML } from "./BusinessList.js";
 
-const contentTarget = document.querySelector('#container')
+const mainTarget = document.querySelector('#container')
+const newYorkTarget = document.querySelector('.businessList--newYork')
+const manufacturingBusinessesTarget = document.querySelector('.businessList--manufacturing')
 
 const businessList = () => {
     const businessArray = getBusinesses()
-    contentTarget.innerHTML = `<h1>Active Businesses</h1>`
+    mainTarget.innerHTML = `<h1>Active Businesses</h1>`
+    //manufacturingBusinessesTarget.innerHTML = `<h1>Manufacturing Businesses</h1>`
 
     businessArray.forEach(
         (companyName) => {
-            contentTarget.innerHTML += businessHTML(companyName)
+            mainTarget.innerHTML += businessHTML(companyName)
         }
     )
 }
-const applicationHTML = businessList()
+const applicationMainHTML = businessList()
+
+const nyBusinessList = () => {
+    const nyBusinessArray = newYorkCompanies()
+    newYorkTarget.innerHTML = `<h1>New York Businesses</h2>`
+
+    nyBusinessArray.forEach(
+        (companyName) => {
+            newYorkTarget.innerHTML += businessHTML(companyName)
+        }
+    )
+}
+const nyApplicationHTML = nyBusinessList()
+
+const manufacturingBusinessList = () => {
+    const manufacturingBusinessArray = manufacturingCompanies()
+    manufacturingBusinessesTarget.innerHTML = `<h1>Manufacturing Businesses</h2>`
+
+    manufacturingBusinessArray.forEach(
+        (companyName) => {
+            manufacturingBusinessesTarget.innerHTML += businessHTML(companyName)
+        }
+    )
+}
+const manufacturingApplicationHTML = manufacturingBusinessList()
 
